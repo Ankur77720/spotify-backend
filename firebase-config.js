2a933ee6a6e22800ed5b2b9169eb013af2f6d3cc
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-03-23 22:22:14 +0530

    fire base private key setup

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin');
-
+const { privateKey } = JSON.parse(process.env.PRIVATE_KEY_FIREBASE);
 
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
         clientEmail: `firebase-adminsdk-dlojn@${process.env.PROJECT_ID}.iam.gserviceaccount.com`,
-        privateKey: `${process.env.PRIVATE_KEY_FIREBASE}`
+        privateKey
     }),
     storageBucket: 'gs://beat-stream.appspot.com' // Replace with your bucket name
 });
